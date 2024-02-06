8fcc95271fddca876fc4149a92d93c85c5dc22e6
pranalidekate111 <pranalidekate111@gmail.com>
2024-02-06 09:45:42 -0800

    Data-Types

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,29 @@
+"use strict"       //treat all js code as newer version.
+
+// alert(3 + 3)   //it is not use because we are using node js not browser.
+
+console.log(3 + 3);
+console.log("Pranali");
+
+// Data-Types:-->
+
+let name = "Pranali"
+let age = 16
+let isLoggedIn = false
+let state;
+
+// primitive  data types:-
+
+// number = > 2 to the power of 53
+// bigint = > for big number
+// string = " "
+// boolean = > true/ false
+// null = > standalone value,null is the representaion of empty value
+// undefined=> the value is not defined yet.
+// symbol = > unique
+
+// typeof :-
+console.log(typeof "pranali") 
+console.log(typeof age) 
+console.log(typeof null) // null is an object, but null return the object.
+console.log(typeof undefined)  // it return undefined
