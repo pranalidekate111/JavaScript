c2cd0b4efcde2354c61d081a334011f4c7df7e63
pranalidekate111 <pranalidekate111@gmail.com>
2024-02-07 04:19:58 -0800

    Comparision Operation on data

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);
+console.log(null < 0);
+
+console.log(undefined < 0); // it always gives false
+console.log(undefined > 0);
+console.log(undefined >= 0);
+console.log(undefined == 0);
+
+// === :-STRICT CHECK:- It strictly check values along with its data type.
+
+
+
