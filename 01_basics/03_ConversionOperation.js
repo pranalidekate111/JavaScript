dc798bfef2bc00b36f68eefbbe990170ca1f1f45
pranalidekate111 <pranalidekate111@gmail.com>
2024-02-07 04:04:28 -0800

    opeartion on data

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -38,5 +38,61 @@ let booleanIsLoggedIn = Boolean(IsLoggedIn)
 
 let someNumber = 33 // this number is converted into string.
 let StringNumber = String(someNumber)
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+
+// ***************************************OPERATIONS************************************************
+
+let value = 3
+let negativeValue = -value // it return -3
+
+console.log(negativeValue)
+
+// same as the upper code
+
+let a = 9
+let b = -a
+console.log(b);
+
+// Basic Operation:-
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+// Concatination of string :-
+
+let str1 = "Hello"
+let str2 = " Pranali"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(1 + "2");
+console.log("1" + 2); 
+console.log("1" + 2 + 2); // if string is present at first then it treated all number as string // it return 122
+console.log(1 + 2 + "2"); // if string ispresent at last then after addition it as is put as it is.// it return 32
+
+console.log(true);
+console.log(+ true); // it return 1
+// console.log(true +); // it gives an error.
+console.log(+""); // it return 0
+
+let gameCounter = 100
+gameCounter++ 
+console.log(gameCounter);
+
+// Postfix increment:-
+let x = 3
+const y = x++
+console.log(x); 
+console.log(y);
+
+// Prefix increment:-
+let x1 = 3
+const y1 = ++x1
+console.log(x1); 
+console.log(y1);
+
